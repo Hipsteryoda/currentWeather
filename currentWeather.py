2b2d92805af757d7e903c148b87ea3431ab7fcfe
Hipsteryoda <ksmith952913@gmail.com>
2024-03-16 15:30:42 -0700

    feat: incl. ability to run as script

--- currentWeather.py
+++ currentWeather.py
@@ -1,24 +1,27 @@
 import json, requests
 from geopy.geocoders import Nominatim
-import SECRETS
 
 # takes arguments of current city or zip code from the command line
 # returns temperature, humidity, 
+# Weather API: https://www.weather.gov/documentation/services-web-api#/
 
 class weatherAPICall:
     
-    def __init__(self, cityAndState, AlertContents = None, PointContents = None,
-                Temp = None, Conditions = None, Lat = None, Lon = None) -> None:
+    def __init__(self, cityAndState) -> None:
         self.cityAndState = cityAndState
-        self.AlertContents = AlertContents
-        self.PointContents = PointContents
-        self.Temp = None
-        self.TempUnit = None
-        self.WindSpeed = None
-        self.WindDirection = None
-        self.Conditions = Conditions
+        self.PointContents = None
+        self.Conditions = None
         self.Lat, self.Lon = self.getLatLon()  
         self.ForecastURL = self.pointsCall(self.Lat, self.Lon)
+        self.getForecast()
+        
+        # UNUSED
+        # self.AlertContents = None
+        # self.Temp = None
+        # self.TempUnit = None
+        # self.WindSpeed = None
+        # self.WindDirection = None
+        # self.Humidity = None
         
     def pointsCall(self, lat, lon):
         url = f"https://api.weather.gov/points/{self.Lat},{self.Lon}"
@@ -42,3 +45,20 @@ class weatherAPICall:
         self.WindSpeed = forecastResponse['properties']['periods'][0]['windSpeed']
         self.WindDirection = forecastResponse['properties']['periods'][0]['windDirection']
         self.Humidity = forecastResponse['properties']['periods'][0]['relativeHumidity']['value']
+
+if __name__ == '__main__':
+    import sys
+    # Get the city and state as the last argument
+    cityAndState = sys.argv[-1]
+    # Instantiate the weather object
+    weather = weatherAPICall(cityAndState)
+    
+    # Print out some details
+    print(f"The current temperature in {cityAndState} is: {weather.Temp}{weather.TempUnit}")
+    print(f"The current wind is: {weather.WindSpeed} from the {weather.WindDirection}")
+    print(f"The current relative humidity is: {weather.Humidity}")
+    
+    
+    
+    
+    
